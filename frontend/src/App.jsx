638c5ca45389bdc50c26a46042da62abc4311f65
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-12-16 11:37:30 +0100

    add react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,33 +6,50 @@ import AdvertisingBar from "./AdvertisingBar";
 import TopBar from "./TopBar";
 import Product from "./components/product";
 import Categories from "./components/Categories";
-function App() {
-  const handleSearch = (searchTerm) => {};
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthSection from "./components/AuthComponents/AuthSection";
 
-  return (
-    <div>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
       <div>
         <div>
-          <TopBar />
-          <Categories />
-          <SearchPanel onSearch={handleSearch} />
-        </div>
-        <div>
+          <div>
+            <TopBar />
+            <Categories />
+            <SearchPanel />
+          </div>
           <AdvertisingBar />
         </div>
-        <div>
-          <AdvertisingBar />
+        <div class="container">
+          <Product source={require("./images/przyklad.jpg")} title="Dacia Logan" price={24099} address="Warszawa" />
+          <Product source={require("./images/przyklad2.png")} title="Samsung używany" price={399} address="Katowice" />
+          <Product source={require("./images/przyklad3.jpg")} title="Koparka LEGO" price={2500} address="Opole" />
+          <Product source={require("./images/przyklad4.jpg")} title="Zegarek Omega" price={35800} address="Poznań" />
+          <Product source={require("./images/przyklad.jpg")} title="Dacia Logan" price={24099} address="Warszawa" />
+          <Product source={require("./images/przyklad2.png")} title="Samsung używany" price={399} address="Katowice" />
+          <Product source={require("./images/przyklad3.jpg")} title="Koparka LEGO" price={2500} address="Opole" />
+          <Product source={require("./images/przyklad4.jpg")} title="Zegarek Omega" price={35800} address="Poznań" />
         </div>
+        <Foot />
       </div>
-      <div class="container">
-        <Product source={require("./images/przyklad.jpg")} title="Dacia Logan" price={24099} address="Warszawa" />
-        <Product source={require("./images/przyklad2.png")} title="Samsung używany" price={399} address="Katowice" />
-        <Product source={require("./images/przyklad3.jpg")} title="Koparka LEGO" price={2500} address="Opole" />
-        <Product source={require("./images/przyklad4.jpg")} title="Zegarek Omega" price={35800} address="Poznań" />
-      </div>
-      <Foot />
-    </div>
-  );
+    ),
+  },
+  {
+    path: "/auth",
+    element: (
+      <>
+        <TopBar />
+        <AuthSection />
+        <Foot />
+      </>
+    ),
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/TopBar.jsx
+++ frontend/src/TopBar.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import './TopBar.css';
-import logo from '../src/images/Logow.png'
-
-
+import React from "react";
+import "./TopBar.css";
+import logo from "../src/images/Logow.png";
+import { Link } from "react-router-dom";
 
 const TopBar = () => {
-    return (
-        <div id="topBar">
-            <div className="left">
-                <img src={logo} alt="Footer Logo" className="footer-logo"/>
-            </div>
-            <div className="right">
-                <button>Dodaj ogłoszenie</button>
-                <button>Wiadomości</button>
-                <button>Zaloguj</button>
-            </div>
-        </div>
-    );
+  return (
+    <div id="topBar">
+      <div className="left">
+        <Link to="/">
+          {" "}
+          <img src={logo} alt="Footer Logo" className="footer-logo" />
+        </Link>
+      </div>
+      <div className="right">
+        <button>Dodaj ogłoszenie</button>
+        <button>Wiadomości</button>
+        <Link to="/auth">
+          {" "}
+          <button>Zaloguj</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
